1eab0b565873f46c388185734c1c783485c05f4c
nomorelonelylife <meme@nomorelonely.life>
2025-01-12 14:42:27 +0100

    bug fix

--- points_bot/bot.py
+++ points_bot/bot.py
@@ -649,7 +649,7 @@ class PointsBot(discord.Client):
                     
                             options.append({
                                 'index': i,
-                                'text': text.strip(),
+                                'option_text': text.strip(),
                                 'points': round(points, 8)
                             })
                         except ValueError:
